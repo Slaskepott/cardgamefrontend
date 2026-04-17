7421a539326518917b62e9d332e6c942ae10417d
Henrik Browne <henrikbrowne@gmail.com>
2026-04-17 14:01:30 +0200

    Added react app

--- react-app/vite.config.js
+++
@@ -1,9 +0,0 @@
-var _a;
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-var repoName = (_a = process.env.GITHUB_REPOSITORY) === null || _a === void 0 ? void 0 : _a.split("/")[1];
-var isPagesBuild = process.env.GITHUB_ACTIONS === "true";
-export default defineConfig({
-    plugins: [react()],
-    base: isPagesBuild && repoName ? "/".concat(repoName, "/") : "/",
-});
