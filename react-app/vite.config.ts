7c702045930b52fd543c2ee5a5a84e380e22eae8
Henrik Browne <henrikbrowne@gmail.com>
2026-04-17 14:13:00 +0200

    Metadata

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const isPagesBuild = process.env.GITHUB_ACTIONS === "true";
 
 export default defineConfig({
   plugins: [react()],
-  base: isPagesBuild && repoName ? `/${repoName}/` : "/",
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : "/",
 });
-
